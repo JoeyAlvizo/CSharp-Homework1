380849c7dc0fac84b628c5581fa0a1895ffa0d40
JoeyAlvizo <joey@joeyalvizo.com>
2021-04-27 23:10:14 -0500

    Upload homework 1

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            // Exercise No.1 - This allows your window to be maximized when application runs
+            WindowState = WindowState.Maximized;
+            uxSubmit.IsEnabled = false;
+        }
+
+        private void uxSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Submitting password:" + uxPassword.Text);
+        }
+        private void keyUpEvent(object sender, KeyEventArgs e)
+        {
+            uxSubmit.IsEnabled = uxName.Text.Length > 0 && uxPassword.Text.Length > 0;
+        }
+    }
+}
